b1e80b09a5424b5bb03161fefe40204c559e0dcf
Ajose Tosin <tosinemidastouch@gmail.com>
2022-07-15 09:53:23 -0700

    02-assembler

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,13 @@
-#include <stdio.h>
+#include<stdio.h>
+
 /**
- * main - entry point
+ * main - Entry point
  *
- * Return: 0 after printing the function
+ * Return: Always 0 (Success)
  */
+int main(void)
 {
-    int mani(void) 
-    {puts("\"Programming is like building a multilingual puzzle");
-	    return (0);
-    }
+	puts("\"Programming is like building a multilingual puzzle");
+
+	return (0);
+}
