26e30470615ea50e4e64c5f5f58577a22a3fa8a8
Ajose Tosin <tosinemidastouch@gmail.com>
2022-07-14 22:15:48 -0700

    Hello

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -4,11 +4,7 @@
  * main -Entry point
  * Return: Always 0 (Success)
  */
- int a;
- long int b;
- long long int c;
- char d;
- float f;
+int main(void)
 }
 printf("size of a char: %lu byte(s)\n", (unsigned long)sizeof(d));
 printf("size of an int: %lu byte(s)\n", (unsigned long)sizeof(a));
