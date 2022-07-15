8d3606176580144e7e0d3fd9d873b672915f1726
Ajose Tosin <tosinemidastouch@gmail.com>
2022-07-15 10:34:18 -0700

    101-quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-#include <unistd.h>
+
 /**
  * main - prints exactly "and that piece of art is useful"
  * - Dora Korpar, 2015-10-19,
@@ -8,6 +8,6 @@
  *   */
 int main(void)
 {
-write(2, "and piece of art is useful\" -Dora Korpar, 2015-10-19\n". 59);
-return (1);
+	write(2, "and piece of art is useful\" -Dora Korpar, 2015-10-19\n". 59);
+	return (1);
 }
