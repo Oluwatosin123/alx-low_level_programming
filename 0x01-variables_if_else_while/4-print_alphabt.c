8cf35e953e18a2d127739a51b122629cd0ceea4c
Ajose Tosin <tosinemidastouch@gmail.com>
2022-09-13 01:25:34 +0100

    finish task 2

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -5,17 +5,13 @@
 int main(void)
 {
   int ch;
-  for (ch = 97; ch <= 122; ch++)
-  {
-    putchar(ch);
+  for (ch = 'a'; ch <= 'z'; ch++)
+  {   
+      if(ch !='q' && ch != 'e')
+      {
+        putchar(ch);
+      }
   }
-
-  putchar(10); /*ascii code for new line*/;
-
-  for (ch =65; ch <= 90; ch++)
-  {
-    putchar(ch);
-  }
-  
+    putchar(10); /*ascii code for new line*/;
     return 0;
 }
